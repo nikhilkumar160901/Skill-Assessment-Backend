79bf49dd8e1f1bba1777606c64f43d512bbdfc2a
Nikhil Kumar <nikhil.kumar@fiellements.com>
2025-08-30 15:33:54 +0530

    remove test from docker

--- src/server.js
+++ src/server.js
@@ -2,7 +2,17 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  async function startServer() {
+    try {
+      await connectDB();      // wait for MySQL
+      await connectRedis();   // wait for Redis
+      app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+    } catch (err) {
+      console.error("Server failed to start:", err);
+      process.exit(1); // optional: exit if DB/Redis not ready
+    }
+  }
 }
 
+startServer();
 module.exports = app;
