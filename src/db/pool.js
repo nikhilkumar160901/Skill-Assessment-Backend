d4e9ef5ac484cd75088cafbd79ea5e6b111886be
Nikhil Kumar <nikhil.kumar@fiellements.com>
2025-08-30 17:08:59 +0530

    console added

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2/promise");
 
+console.log("Connecting to the database...", process.env.DB_HOST, process.env.DB_USER, process.env.DB_NAME, process.env.DB_PORT, process.env.DB_PASSWORD);
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
